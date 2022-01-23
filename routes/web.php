14e02a52fe9af424acf53fdc962330fbd55d534d
LukGaming <thelukgaming333@gmail.com>
2022-01-23 11:58:31 -0300

    Pegando dados do perfil do usuário

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,7 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
-use App\Http\Controllers\ProdutosController;
 use App\Http\Controllers\UserController;
-use App\Http\Controllers\fileUploadsController;
-use App\Http\Controllers\UploadImagesProductController;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -15,7 +12,7 @@ use App\Http\Controllers\UploadImagesProductController;
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->post('register', 'AuthController@register');
@@ -23,24 +20,25 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('user/{id}', 'UserController@getUserById');
 
     /*Rotas de Produtos*/
-    $router->get('/produtos',  'ProdutosController@index');
-    $router->post('/produtos',  'ProdutosController@store');
-    $router->get('/produtos/{id}',  'ProdutosController@edit');
-    $router->patch('/produtos/{id}',  'ProdutosController@update');
-    $router->delete('/produtos/{id}',  'ProdutosController@destroy');
+    $router->get('/produtos', 'ProdutosController@index');
+    $router->post('/produtos', 'ProdutosController@store');
+    $router->get('/produtos/{id}', 'ProdutosController@edit');
+    $router->patch('/produtos/{id}', 'ProdutosController@update');
+    $router->delete('/produtos/{id}', 'ProdutosController@destroy');
     /*Rotas de Produtos*/
 
     //Rotas de Categorias
-    $router->get('/categorias',  'CategoriasController@index');
-    $router->post('/categorias',  'CategoriasController@store');
-    $router->get('/categorias/{id}',  'CategoriasController@edit');
-    $router->patch('/categorias/{id}',  'CategoriasController@update');
-    $router->delete('/categorias/{id}',  'CategoriasController@destroy');
+    $router->get('/categorias', 'CategoriasController@index');
+    $router->post('/categorias', 'CategoriasController@store');
+    $router->get('/categorias/{id}', 'CategoriasController@edit');
+    $router->patch('/categorias/{id}', 'CategoriasController@update');
+    $router->delete('/categorias/{id}', 'CategoriasController@destroy');
     //Rotas de Categorias
     $router->post('/files', 'UploadImagesProductController@upload_image_produto');
-    $router->get('imagens/produtos/{id_produto}' ,'UploadImagesProductController@list_images_produto');
-    $router->delete('imagens/produtos/{id_imagem}' ,'UploadImagesProductController@remove_image_producto');
+    $router->get('imagens/produtos/{id_produto}', 'UploadImagesProductController@list_images_produto');
+    $router->delete('imagens/produtos/{id_imagem}', 'UploadImagesProductController@remove_image_producto');
 
     // Rotas para perfil do usuário
-    $router->post('/perfil' ,'PerfilUserController@store');
+    $router->post('/perfil', 'PerfilUserController@store');
+    $router->get('/perfil/{id}', 'PerfilUserController@edit');
 });
